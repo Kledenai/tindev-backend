429505e0aad45469a533db432822556c6bda9ac6
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 02:12:50 -0300

    adicionando o routes no server.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const routes = express.Router();
 
-server.get('/', (req, res) => {
+routes.get('/', (req, res) => {
     return res.json({ message: `Olá ${req.query.name}` });
 });
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const routes = require('./routes');
 
 const server = express();
 
-
+server.use(routes);
 
 server.listen(3333);
