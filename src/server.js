b3a3b09f475b836a3cac9c0baac96089efd22f6c
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 02:42:49 -0300

    adicionando o mongodb

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://kledenai:qRVepHoNwAE5zC82@cluster0-rhehb.mongodb.net/tinder-server?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 
