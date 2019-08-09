f30cd1ffa7b4930adaa10367305341464ac27e1d
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 04:17:52 -0300

    backend 99%

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,9 @@ const LikeController = require('./controllers/LikeController');
 
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
 routes.post('/devs/:devId/dislikes', DeslikeConroller.store);
 routes.post('/devs/:devId/likes', LikeController.store);
 
