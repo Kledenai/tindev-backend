122920455a7d6251d98f3024c7d95b4a22cd27e9
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 03:14:52 -0300

    adicionando função para não duplicar usuário

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,11 +1,31 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 
 module.exports = {
-    store(req, res){
+    async store(req, res){
         const { username } = req.body;
 
-        const response = axios.get(`https://api.github.com/users/${username}`);
+        const userExist = await Dev.findOne({ user: username });
 
-        return res.json(response.data);
+        if(userExist) {
+            return res.json(userExist)
+        }
+
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+
+        const { 
+            name, 
+            bio, 
+            avatar_url: avatar 
+        } = response.data;
+
+        const dev = await Dev.create({ 
+            name,
+            user: username,
+            bio,
+            avatar: avatar
+        })
+
+        return res.json(dev);
     }
 }
