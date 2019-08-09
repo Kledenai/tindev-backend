e19ea2171309580e29193be04475839c68e0e118
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-09 04:24:08 -0300

    adicionando o cors para ser acessado por qualquer aplicação

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const server = express();
 
 mongoose.connect('mongodb+srv://kledenai:qRVepHoNwAE5zC82@cluster0-rhehb.mongodb.net/tinder-server?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
+
+server.use(cors());
 
 server.use(express.json());
 
