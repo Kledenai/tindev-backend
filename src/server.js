900dee7bc42e7c0c0905725ba9c7ddb6bc0d1c3b
Kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-11 15:39:40 -0300

    mudando o server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const io = require('socket.io');
 const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
 
 mongoose.connect('mongodb+srv://kledenai:qRVepHoNwAE5zC82@cluster0-rhehb.mongodb.net/tinder-server?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
